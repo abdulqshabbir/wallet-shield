f49eaefbf636e015417d982284a3cfc13a95625e
Hassan Shabbir <hassan149367@gmail.com>
2022-03-04 14:13:20 -0500

    added icons to sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,18 +1,32 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faCircleChevronDown, faTree, faMoneyBill1Wave, faChartLine, faBuildingColumns, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 function Sidebar() {
 	 return (
 		 <React.Fragment>
-			<div className="sidenav invisible xl:visible text-xl text-blue-200 bg-blue-900">
-				<a href="#a">Abdul's Budget
+			<div className="sidenav invisible xl:visible text-xl text-blue-200 bg-blue-900 pt-3">
+				<a className="mb-2" href="#a">
+					<FontAwesomeIcon className="text-lg pr-3" icon={faTree} />
+					Abdul's Budget
 					<FontAwesomeIcon className="text-lg pl-3" icon={faCircleChevronDown} />
 				</a>
-				<a href="#a">Budget</a>
-				<a href="#a">Reports</a>
-				<a href="#a">All Accounts</a>
-				<a href="#a">Add Accounts</a>
+				<a className="mb-2" href="#a">
+					<FontAwesomeIcon className="text-lg pr-3" icon={faMoneyBill1Wave} />
+				Budget
+			  </a>
+				<a className="mb-2" href="#a">
+					<FontAwesomeIcon className="text-lg pr-3" icon={faChartLine} />
+					Reports
+				</a>
+				<a className="mb-2" href="#a">
+					<FontAwesomeIcon className="text-lg pr-3" icon={faBuildingColumns} />
+					All Accounts
+				</a>
+				<a className="mb-2" href="#a">
+					<FontAwesomeIcon className="text-lg pr-3" icon={faCirclePlus} />
+					Add Accounts
+				</a>
 			</div>
 		 </React.Fragment>
 	 )
