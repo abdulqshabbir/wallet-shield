6f3210dfbcbc8c4e6976636cb8ee7c0ae3fed572
Hassan Shabbir <hassan149367@gmail.com>
2022-03-04 19:04:56 -0500

    added icons to sm-lg footer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -29,7 +29,7 @@ function Header() {
 				<header className="grid grid-cols-3 justify-items-center py-6">
 					<FontAwesomeIcon className="hover:text-blue-300 text-xl sm:text-2xl md:text-3xl pt-2" icon={faEllipsis} />
 					<h1 className="hover:text-blue-300">
-						<span className="text-3xl sm:text-4xl md:text-5xl">Feb 2022</span>
+						<span className="text-3xl sm:text-4xl md:text-5xl">Feb&nbsp;2022</span>
 						<FontAwesomeIcon className="text-xl sm:text-2xl md:text-3xl pb-1 md:pb-2 pl-2 sm:pl-3 md:pl-4 lg:pl-6" icon={faCircleChevronDown} />
 					</h1>
 					<FontAwesomeIcon className="hover:text-blue-300 text-xl sm:text-2xl md:text-3xl pt-2" icon={faPenToSquare} />
