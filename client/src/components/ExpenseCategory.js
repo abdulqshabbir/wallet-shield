272d31246538feb2c6374a7c3554e5fa07245eb0
Hassan Shabbir <hassan149367@gmail.com>
2022-03-06 20:15:24 -0500

    change to baby blue theme

--- client/src/components/ExpenseCategory.js
+++ client/src/components/ExpenseCategory.js
@@ -4,7 +4,7 @@ import Expense from "./Expense";
 function ExpenseCategory({ categoryName }) {
     return(
         <React.Fragment>
-						<div className="expense-category xl:pl-56 xl:pr-10 grid grid-cols-2 justify-items-center pt-10 pb-3 gap-y-4">
+						<div className="expense-category xl:pl-56 xl:pr-10 grid grid-cols-2 justify-items-center pt-6 pb-3 gap-y-4">
 							<h2 className="font-semibold">{categoryName}</h2>
 							<h2 className="font-semibold">Amount Spent</h2>
 						</div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,16 +5,16 @@ import React from 'react'
 function Header() {
 	 return (
 		 <React.Fragment>
-				<header className="xl:pl-56 xl:pr-10 header grid grid-cols-5 lg:grid-cols-3 justify-items-center py-4">
-					<FontAwesomeIcon className="cursor-pointer pt-1 hover:text-blue-500" icon={faEllipsis} />
-					<h1 className="col-span-3 lg:col-span-1 cursor-pointer hover:text-blue-500">
+				<header className="header xl:pl-56 xl:pr-10 grid grid-cols-5 lg:grid-cols-3 justify-items-center pt-4 pb-2 bg-blue-200 ring-1">
+					<FontAwesomeIcon className="cursor-pointer pt-1 hover:text-blue-400" icon={faEllipsis} />
+					<h1 className="col-span-3 lg:col-span-1 cursor-pointer hover:text-blue-400">
 						<span className="">Feb&nbsp;2022</span>
 						<FontAwesomeIcon className="pl-2" icon={faCircleChevronDown} />
 					</h1>
-					<FontAwesomeIcon className="cursor-pointer pt-2 hover:text-blue-500" icon={faPenToSquare} />
+					<FontAwesomeIcon className="cursor-pointer pt-2 hover:text-blue-400" icon={faPenToSquare} />
 				</header>
 		
-				<section className="xl:pl-56 xl:pr-10 flex justify-center items-center">
+				<section className="xl:pl-56 xl:pr-10 pt-4 flex justify-center items-center hover:text-blue-400">
 					<h2 className="mr-4">Assign your money</h2>
 					<FontAwesomeIcon className="" icon={faArrowRight} />
 				</section>
