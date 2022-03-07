6050ae67e85a33b28684d21e7632610a471b18d1
Hassan Shabbir <hassan149367@gmail.com>
2022-03-06 20:15:24 -0500

    change to baby blue theme

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,20 +5,20 @@ import React from 'react'
 function Footer() {
 	 return (
 		 <React.Fragment>
-			 <div className="footer xl:invisible grid grid-cols-5 justify-content-center content-center justify-around px-12 fixed bottom-0 w-full h-14 sm:h-16 z-10 bg-gray-400">
-					<a className="hover:text-blue-500 text-center" href="#a">
+			 <div className="footer xl:invisible grid grid-cols-5 justify-content-center content-center justify-around px-12 fixed bottom-0 w-full h-14 sm:h-16 z-10 bg-blue-200 ring-1">
+					<a className="hover:text-blue-400 text-center" href="#a">
 						<FontAwesomeIcon className="" icon={faCircleChevronDown} />
 					</a>
-					<a className="hover:text-blue-500 text-center" href="#a">
+					<a className="hover:text-blue-400 text-center" href="#a">
 						<FontAwesomeIcon className="" icon={faMoneyBill1Wave} />
 					</a>
-					<a className="hover:text-blue-500 text-center" href="#a">
+					<a className="hover:text-blue-400 text-center" href="#a">
 						<FontAwesomeIcon className="" icon={faChartLine} />
 					</a>
-					<a className="hover:text-blue-500 text-center" href="#a">
+					<a className="hover:text-blue-400 text-center" href="#a">
 						<FontAwesomeIcon className="" icon={faBuildingColumns} />
 					</a>
-					<a className="hover:text-blue-500 text-center" href="#a">
+					<a className="hover:text-blue-400 text-center" href="#a">
 						<FontAwesomeIcon className="" icon={faCirclePlus} />
 					</a>
 				</div>
