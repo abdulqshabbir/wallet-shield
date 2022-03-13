154c8e0b10426dfd925d33bf01a7b95a9d5683f5
Hassan Shabbir <hassan149367@gmail.com>
2022-03-12 23:07:17 -0500

    style input fields

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -25,18 +25,24 @@ export default function Test() {
 
 	return (
 		<React.Fragment>
-            <div className="xl:pl-56 xl:pr-10 grid grid-cols-2 justify-items-center pt-6 pb-3 gap-y-4">
-                <label htmlFor="expenseName">Expense name: </label>
-                <input type="text" name="expenseName" value={name} onChange={(e) => setName(e.target.value)} className="b border-2 bg-gray-100"/>
+			<div className="xl:pl-56 xl:pr-10 grid grid-cols-2 justify-items-center pt-6 pb-3 gap-y-4 bg-blue-100">
+				<label htmlFor="expenseName">Expense name: </label>
+				<input type="text" name="expenseName" value={name} onChange={(e) => setName(e.target.value)} 
+					className="w-4/5 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
 
-                <label htmlFor="expenseAmount">Expense amount: </label>
-                <input type="text" name="expenseAmount" onChange={e => setAmount(e.target.value)} className="b border-2 bg-gray-100"/>
+				<label htmlFor="expenseAmount">Expense amount: </label>
+				<input type="number" step="any" name="expenseAmount" onChange={e => setAmount(e.target.value)} 
+					className="w-4/5 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
 
-                <label htmlFor="expenseMax">Expense Max: </label>
-                <input type="text" name="expenseMax" onChange={e => setMax(e.target.value)} className="b border-2 bg-gray-100"/>
+				<label htmlFor="expenseMax">Expense max: </label>
+				<input type="number" step="any" name="expenseMax" onChange={e => setMax(e.target.value)} 
+					className="w-4/5 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
 
-                <button type="submit" className="bg-gray-100 p-4" onClick={e => createNewExpense(e)}>Submit</button>
-            </div>
+				<div></div>
+				<button type="submit" className="bg-blue-200 ring-1 rounded-md ring-blue-500 px-4 py-2" 
+					onClick={e => createNewExpense(e)}>Submit</button>
+
+			</div>
 		</React.Fragment>
 	)
 }
