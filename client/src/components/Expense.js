2f075a08576276e13b9ffaba0decce7fd5a6896d
Hassan Shabbir <hassan149367@gmail.com>
2022-03-12 23:53:48 -0500

    changed overspent and spent styling

--- client/src/components/Expense.js
+++ client/src/components/Expense.js
@@ -1,13 +1,24 @@
 import React from "react"
 
 function Expense({ expenseName, expenseAmount, expenseMax }) {
-	const width = (expenseAmount / expenseMax * 100) + '%'
+	var style
+	var width
+	if (expenseAmount > expenseMax) {
+		style = ["font-thick bg-red-200 ring-red-600", "bg-red-300 text-red-600"]
+		width = ((expenseAmount - expenseMax ) / expenseMax * 100) + '%'
+	} else {
+		style = ["font-thin bg-blue-300 ring-blue-800", "bg-blue-200 text-gray-800"]
+		width = (expenseAmount / expenseMax * 100) + '%'
+	}
+
 	return (
 		<React.Fragment>
 			<div className="expense xl:pl-56 xl:pr-10 grid grid-cols-2 py-3 justify-items-center">
 				<p className="justify-self-center font-light">{expenseName}</p>
-				<div className="w-5/6 font-thin bg-gray-200 rounded-md ring-1 ring-blue-800">
-						<div style={{width: width}} className="py-1 pl-1 ring-1 rounded-md bg-blue-300 text-gray-800">${expenseAmount}/${expenseMax}</div>
+				<div className={style[0] + " w-5/6 rounded-md ring-1"}>
+						<div style={{width: width}} className={style[1] + " py-1 pl-1 ring-1 rounded-md"}>
+							${expenseAmount}/${expenseMax}
+						</div>
 				</div>
 			</div>
 		</React.Fragment>
--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -28,15 +28,15 @@ export default function Test() {
 			<div className="xl:pl-56 xl:pr-10 grid grid-cols-2 justify-items-center pt-6 pb-3 gap-y-4 bg-blue-100">
 				<label htmlFor="expenseName">Expense name: </label>
 				<input type="text" name="expenseName" value={name} onChange={(e) => setName(e.target.value)} 
-					className="w-4/5 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
+					className="w-5/6 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
 
 				<label htmlFor="expenseAmount">Expense amount: </label>
 				<input type="number" step="any" name="expenseAmount" onChange={e => setAmount(e.target.value)} 
-					className="w-4/5 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
+					className="w-5/6 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
 
 				<label htmlFor="expenseMax">Expense max: </label>
 				<input type="number" step="any" name="expenseMax" onChange={e => setMax(e.target.value)} 
-					className="w-4/5 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
+					className="w-5/6 bg-blue-200 ring-1 rounded-md ring-blue-500 p-1"/>
 
 				<div></div>
 				<button type="submit" className="bg-blue-200 ring-1 rounded-md ring-blue-500 px-4 py-2" 
