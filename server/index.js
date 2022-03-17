8ee06072d6b3aa8451556b64625367cf0a0e7916
Hassan Shabbir <hassan149367@gmail.com>
2022-03-16 23:31:30 -0400

    format

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 try {
-    await db.authenticate()
-    console.log('Connection has been established successfully.')
+	await db.authenticate()
+	console.log('Connection has been established successfully.')
 } catch (error) {
-    console.error('Unable to connect to the database:', error)
+	console.error('Unable to connect to the database:', error)
 }
 
 
 // This runs the DROP TABLE IF EXISTS query
 (async () => {
-    await db.sync({ force: true })
+	await db.sync({ force: true })
 })()
 
 // allow all cross origin requests
@@ -29,29 +29,29 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/expenses", (req, res) => {
-    Expense.findAll()
-    .then(expenses => {
-        res.status(200).json(expenses)
-    })
-    .catch(e => {
-        console.log(e)
-    })
+	Expense.findAll()
+		.then(expenses => {
+			res.status(200).json(expenses)
+		})
+		.catch(e => {
+			console.log(e)
+		})
 })
 
 app.post("/expenses", (req, res) => {
-    Expense.create({
-        expenseName: req.body.expenseName,
-        expenseAmount: req.body.expenseAmount,
-        expenseMax: req.body.expenseMax
-    })
-    .then(expense => {
-        res.status(200).send(expense)
-    })
-    .catch(e => {
-        console.log(e)
-    })
+	Expense.create({
+		expenseName: req.body.expenseName,
+		expenseAmount: req.body.expenseAmount,
+		expenseMax: req.body.expenseMax
+	})
+		.then(expense => {
+			res.status(200).send(expense)
+		})
+		.catch(e => {
+			console.log(e)
+		})
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+	console.log(`Server listening on port ${PORT}`)
 })
