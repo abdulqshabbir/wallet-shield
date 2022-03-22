a3464b7feebc53e72d6f8a444e8bb6483d8e364f
Hassan Shabbir <hassan149367@gmail.com>
2022-03-22 18:49:41 -0400

    add footer to categories route

--- client/src/routes/Categories.js
+++ client/src/routes/Categories.js
@@ -1,12 +1,19 @@
 import React from "react"
-import Header from '../components/Header'
 import Sidebar from "../components/Sidebar"
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Categories({  }) {
 	return (
 		<React.Fragment>
 			<Sidebar/>
-			<Header/>
+			<div className="h-screen">
+				<Header/>
+				<main>
+
+				</main>
+				<Footer/>
+			</div>
 		</React.Fragment>
 	)
 }
