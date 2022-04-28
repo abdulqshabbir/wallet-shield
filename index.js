074e9b201778b2e3c0e961c7078946cfceecf723
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-04-28 18:57:13 +0800

    compelted POST route handler for /login

--- index.js
+++ index.js
@@ -190,8 +190,29 @@ app.post("/api/signup", async(req, res) => {
 	} catch(e) {
 		return res.status(500).json({ error: "Internal server error."})
 	}
+})
 
+app.post("/api/login", async (req, res) => {
+	const { email, password } = req.body
 
+	if (!email || !password) {
+		return res.status(400).json({ error: "Please fill all required fields."})
+	}
+	try {
+		let user = await User.findOne({ where: { email: email }})
+		if (!user) {
+			return res.status(400).json({ error: "User with this email does not exist."})
+		}
+
+		let isPasswordMatch = await bcrypt.compare(password, user.hashedPassword)
+		if (!isPasswordMatch) {
+			return res.status(400).json({ error: "Incorrect password for this email."})
+		} else {
+			return res.status(200).json(user)
+		}
+	} catch(e) {
+		return res.status(500).json({ error: "Internal server error."})
+	}
 })
 
 app.listen(PORT, () => {
