86257315417316b805de5525b5bfb6eb9ca63e4e
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-05-08 15:35:25 +0800

    UI for adding categories moved to index page

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,6 +1,6 @@
-export default function Button({ onClick, children }) {
+export default function Button({ onClick, width, children }) {
     return (
-        <button onClick={onClick} className="w-[100%] h-14 flex justify-center items-center font-normal text-[22px] rounded-lg text-white bg-primaryBlue">
+        <button onClick={onClick} className={`w-[${width? width:'100%'}] h-14 flex justify-center items-center font-normal text-[22px] rounded-lg text-white bg-primaryBlue`}>
             {children}
         </button>
     )
--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,10 +1,10 @@
 import { Oval } from "react-loader-spinner";
 
-export default function Spinner() {
+export default function Spinner({ height, width }) {
     return (
         <Oval
-            height={`2.5rem`}
-            width={`2.5rem`}
+            height={height ? height: `2.5rem`}
+            width={width? width: `2.5rem`}
             ariaLabel="loading-indicator"
             strokeWidthSecondary={4}
             color="white"
