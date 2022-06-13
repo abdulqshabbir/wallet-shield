16671dfe23c0430fc7cb40fd9a0f611dcc761010
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-06-13 10:55:05 +0800

    removed dots menu on large screens

--- client/src/components/Shared/Header.js
+++ client/src/components/Shared/Header.js
@@ -3,9 +3,11 @@ import { FaHome } from "react-icons/fa"
 import React from "react"
 import { useModal } from "../../contexts/Modal"
 import { Link } from "react-router-dom"
+import useWindowDimensions from "../../contexts/useWindowDimensions"
 
 function Header() {
 	const [modal, setModal] = useModal()
+	const { width, } = useWindowDimensions()
 	 return (
 		 <React.Fragment>
 				<header className="header flex justify-between items-center px-4 py-4 border-b-[1px]">
@@ -15,10 +17,13 @@ function Header() {
 							<span className="ml-2 font-medium">April&nbsp;2022</span>
 						</div>
 					</Link>
-					<TbDotsCircleHorizontal
-						className="cursor-pointer"
-						onClick={() => setModal(true)}
-					/>
+					{
+						width < 1024 && 
+						<TbDotsCircleHorizontal
+							className="cursor-pointer"
+							onClick={() => setModal(true)}
+						/>
+					}
 				</header>
 		 </React.Fragment>
 	 )
